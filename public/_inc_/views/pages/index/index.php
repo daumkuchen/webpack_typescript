81f70e58fa275dfc69539e6b60ab35906c484d62
nojima kosuke <nodima05@gmail.com>
2019-03-08 16:56:23 +0900

    delete: php, stage.js, vendor.bundle.js

--- public/_inc_/views/pages/index/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses( 'Request', 'http' );
-$current = Request::getRequestURI();
-$map = App::config( 'site.map' );
-$site_data = App::config( 'site.data' );; ?>
-
-<div id="top" class="page-content">
-    <div id="stage"></div>
-</div>
